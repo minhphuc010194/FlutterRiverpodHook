dc9c8e93d660f4c7c1a4ce460419a16a8f6ca251
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2022-07-03 11:13:20 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod_hook/counter.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// final countProvider = StateProvider<int>((ref) => 0);
+final getCountPrefProvider = FutureProvider.autoDispose<int>((ref) async {
+  final prefs = await SharedPreferences.getInstance();
+  final value = prefs.getInt('counter') ?? 0;
+  print(value);
+  return value;
+});
+
+final saveCountPrefProvider =
+    FutureProvider.autoDispose.family<void, int>((ref, count) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setInt('counter', count);
+  ref.refresh(getCountPrefProvider);
+});
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends HookConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Example')),
+        body: const Center(
+          child: Counter(),
+        ),
+      ),
+    );
+  }
+}
